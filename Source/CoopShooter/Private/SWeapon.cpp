4f5a16559206676aa2d54b93301f327738e2b73d
Adam <Twilightsparkleadam@gmail.com>
2018-07-09 19:17:48 +0300

    Added teams and friendly fire

--- Source/CoopShooter/Private/SWeapon.cpp
+++ Source/CoopShooter/Private/SWeapon.cpp
@@ -100,7 +100,7 @@ void ASWeapon::Fire()
 				ActualDamage *= 4.0f;
 			}
 
-			UGameplayStatics::ApplyPointDamage(HitActor, ActualDamage, ShotDirection, Hit, MyOwner->GetInstigatorController(), this, DamageType);
+			UGameplayStatics::ApplyPointDamage(HitActor, ActualDamage, ShotDirection, Hit, MyOwner->GetInstigatorController(), MyOwner, DamageType);
 			
 			PlayImpactEfffects(SurfaceType, Hit.ImpactPoint);
 
